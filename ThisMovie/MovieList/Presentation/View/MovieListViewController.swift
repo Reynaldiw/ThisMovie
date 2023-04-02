947f7b36a49d18cb7539406021d2b90ecd0337c0
Reynaldi <reynaldiwijaya2306@gmail.com>
2023-04-02 14:48:43 +0700

    Simulate to fetch Genres from API and implement delegate and datasource to `TableView` to show list of genre.

--- ThisMovie/MovieList/Presentation/View/MovieListViewController.swift
+++ ThisMovie/MovieList/Presentation/View/MovieListViewController.swift
@@ -7,4 +7,71 @@
 
 import UIKit
 
-final class MovieListViewController: UITableViewController {}
+final class MovieListViewController: UITableViewController {
+    
+    private var genreModel = [GenreItem]()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        loadGenres()
+    }
+    
+    private func loadGenres() {
+        DispatchQueue.main.asyncAfter(deadline: .now() + 2.0) {
+            self.genreModel = GenreItem.prototypeData
+            self.tableView.reloadData()
+        }
+    }
+}
+
+extension MovieListViewController {
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = MovieGenreCell()
+        cell.configureView(with: genreModel)
+        
+        return cell
+    }
+    
+    override func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 72
+    }
+}
+
+private extension MovieGenreCell {
+    func configureView(with genreModel: [GenreItem]) {
+        configureView()
+        genreCollectionModel.removeAll()
+        genreCollectionModel = genreModel.toViewModels()
+        genreCollectionView.reloadData()
+    }
+}
+
+private extension GenreItem {
+    static var prototypeData: [GenreItem] {
+        return [
+            GenreItem(id: 28, name: "Action"),
+            GenreItem(id: 28, name: "Adventure"),
+            GenreItem(id: 28, name: "Animation"),
+            GenreItem(id: 28, name: "Comedy"),
+            GenreItem(id: 28, name: "Crime"),
+            GenreItem(id: 28, name: "Documentary"),
+            GenreItem(id: 28, name: "Drama"),
+            GenreItem(id: 28, name: "Family")
+        ]
+    }
+}
+
+private extension Collection where Element == GenreItem {
+    func toViewModels() -> [MovieGenreViewModel] {
+        return map { .init(name: $0.name) }
+    }
+}
