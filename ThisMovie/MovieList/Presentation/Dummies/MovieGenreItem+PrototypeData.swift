be59ca964a0e8a4b776ac8afc52c4e20572e24b9
Reynaldi <reynaldiwijaya2306@gmail.com>
2023-04-02 17:51:48 +0700

    Move `MovieGenreItem.prototypeData` into it's own file.

---
+++ ThisMovie/MovieList/Presentation/Dummies/MovieGenreItem+PrototypeData.swift
@@ -0,0 +1,23 @@
+//
+//  MovieGenreItem+PrototypeData.swift
+//  ThisMovie
+//
+//  Created by Reynaldi on 02/04/23.
+//
+
+import Foundation
+
+extension MovieGenreItem {
+    static var prototypeData: [MovieGenreItem] {
+        return [
+            MovieGenreItem(id: 28, name: "Action"),
+            MovieGenreItem(id: 28, name: "Adventure"),
+            MovieGenreItem(id: 28, name: "Animation"),
+            MovieGenreItem(id: 28, name: "Comedy"),
+            MovieGenreItem(id: 28, name: "Crime"),
+            MovieGenreItem(id: 28, name: "Documentary"),
+            MovieGenreItem(id: 28, name: "Drama"),
+            MovieGenreItem(id: 28, name: "Family")
+        ]
+    }
+}
--- ThisMovie/MovieList/Presentation/View/MovieListViewController.swift
+++ ThisMovie/MovieList/Presentation/View/MovieListViewController.swift
@@ -69,18 +69,3 @@ private extension MovieGenreCell {
         genreCollectionView.reloadData()
     }
 }
-
-private extension MovieGenreItem {
-    static var prototypeData: [MovieGenreItem] {
-        return [
-            MovieGenreItem(id: 28, name: "Action"),
-            MovieGenreItem(id: 28, name: "Adventure"),
-            MovieGenreItem(id: 28, name: "Animation"),
-            MovieGenreItem(id: 28, name: "Comedy"),
-            MovieGenreItem(id: 28, name: "Crime"),
-            MovieGenreItem(id: 28, name: "Documentary"),
-            MovieGenreItem(id: 28, name: "Drama"),
-            MovieGenreItem(id: 28, name: "Family")
-        ]
-    }
-}
