ff6473db5d2b8a9d39912f4c0a287f1bc6422ee6
Reynaldi <reynaldiwijaya2306@gmail.com>
2023-04-02 14:46:24 +0700

    Use `NSLayoutConstraint` to configure constraint of `CollectionView` to superview, and implement delegate and datasource of `CollectionView` to show genres movie.

--- ThisMovie/MovieList/Presentation/View/Genre/MovieGenreCell.swift
+++ ThisMovie/MovieList/Presentation/View/Genre/MovieGenreCell.swift
@@ -13,24 +13,78 @@ struct MovieGenreViewModel {
 
 final class MovieGenreCell: UITableViewCell {
     
-    private var genreCollectionView: UICollectionView = {
+    public var genreCollectionModel = [MovieGenreViewModel]()
+    
+    public var genreCollectionView: UICollectionView = {
         let layout = UICollectionViewFlowLayout()
         layout.scrollDirection = .horizontal
-        
-        let collectionView = UICollectionView()
-        collectionView.collectionViewLayout = layout
+        layout.estimatedItemSize = .zero
+        let collectionView = UICollectionView(frame: .zero, collectionViewLayout: layout)
         collectionView.showsHorizontalScrollIndicator = false
+        collectionView.translatesAutoresizingMaskIntoConstraints = false
+        
         return collectionView
     }()
     
-    override func awakeFromNib() {
-        super.awakeFromNib()
+    public func configureView() {
+        contentView.addSubview(genreCollectionView)
+        NSLayoutConstraint.activate([
+            genreCollectionView.topAnchor.constraint(equalTo: contentView.topAnchor),
+            genreCollectionView.leadingAnchor.constraint(equalTo: contentView.leadingAnchor),
+            genreCollectionView.trailingAnchor.constraint(equalTo: contentView.trailingAnchor),
+            genreCollectionView.bottomAnchor.constraint(equalTo: contentView.bottomAnchor),
+        ])
         
-        configureView()
+        genreCollectionView.register(GenreCell.self, forCellWithReuseIdentifier: GenreCell.identifier)
+        genreCollectionView.delegate = self
+        genreCollectionView.dataSource = self
+    }
+}
+
+extension MovieGenreCell: UICollectionViewDelegate, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return genreCollectionModel.count
     }
     
-    private func configureView() {
-        genreCollectionView.frame = bounds
-        addSubview(genreCollectionView)
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        guard genreCollectionModel.count > indexPath.item else { return UICollectionViewCell() }
+        
+        let genre = genreCollectionModel[indexPath.item]
+        let cell: GenreCell = collectionView.dequeueReusableCell(at: indexPath)
+        cell.configureView(with: genre.name)
+        
+        return cell
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        return CGSize(width: 100, height: 30)
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, insetForSectionAt section: Int) -> UIEdgeInsets {
+        return UIEdgeInsets(top: 16, left: 16, bottom: 16, right: 16)
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
+        return 8
+    }
+}
+
+private extension GenreCell {
+    func configureView(with genreName: String) {
+        configureView()
+        genreLabel.text = genreName
+    }
+}
+
+extension UICollectionViewCell {
+    static var identifier: String {
+        return String(describing: self)
+    }
+}
+
+extension UICollectionView {
+    func dequeueReusableCell<T: UICollectionViewCell>(at indexPath: IndexPath) -> T {
+        let identifier = T.identifier
+        return dequeueReusableCell(withReuseIdentifier: identifier, for: indexPath) as! T
     }
 }
