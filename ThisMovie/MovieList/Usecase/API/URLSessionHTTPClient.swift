3491efc27b70c823e25dbad54244f24310e5cfbe
Reynaldi <reynaldiwijaya2306@gmail.com>
2023-04-03 22:43:13 +0700

    Create static helper method for mapping result data from API to avoid redundant method, since method protocol has the same Result.

--- ThisMovie/MovieList/Usecase/API/URLSessionHTTPClient.swift
+++ ThisMovie/MovieList/Usecase/API/URLSessionHTTPClient.swift
@@ -10,54 +10,31 @@ import Foundation
 final class URLSessionHTTPClient: MovieListHTTPClient, MovieGenreListHTTPClient {
     struct UnexpectedValuesRepresentation: Error {}
     
+    typealias Result = Swift.Result<(Data, HTTPURLResponse), Error>
+    
     func getMovie(from url: URL, completion: @escaping (MovieListHTTPClient.Result) -> Void) {
-        URLSessionHTTPClient.performGETFetch(to: url) { data, response, error in
-            completion(Result{
-                if let error = error {
-                    throw error
-                } else if let data, let response = response as? HTTPURLResponse {
-                    return (data, response)
-                } else {
-                    throw UnexpectedValuesRepresentation()
-                }
-            })
-        }
+        URLSessionHTTPClient.performGETFetch(to: url) { completion($0) }
     }
     
     func getMovieGenre(from url: URL, completion: @escaping (MovieGenreListHTTPClient.Result) -> Void) {
-        URLSessionHTTPClient.performGETFetch(to: url) { data, response, error in
-            completion(Result{
-                if let error = error {
-                    throw error
-                } else if let data, let response = response as? HTTPURLResponse {
-                    return (data, response)
-                } else {
-                    throw UnexpectedValuesRepresentation()
-                }
-            })
-        }
+        URLSessionHTTPClient.performGETFetch(to: url) { completion($0) }
     }
     
-    private static func performGETFetch(to url: URL, completion: @escaping (Data?, URLResponse?, Error?) -> Void) {
+    private static func performGETFetch(to url: URL, completion: @escaping (Result) -> Void) {
         URLSession.shared.dataTask(with: url) { data, response, error in
-            completion(data, response, error)
+            completion(map(data: data, response: response, error: error))
         }.resume()
     }
     
-//    private static createURL(_ url: URL, request: [String: Any]) -> URL? {
-//        var urlComponent = URLComponents(string: url.absoluteString)
-//        urlComponent?.queryItems = createQueries(with: request)
-//
-//        return urlComponent?.url
-//    }
-//
-//    private static func createQueries(with request: [String: Any]) -> [URLQueryItem] {
-//        let request = request.mapValues { $0 as? String }.compactMapValues { $0 }
-//        var queryItems = [URLQueryItem]()
-//        request.forEach { key, value in
-//            queryItems.append(URLQueryItem(name: key, value: value))
-//        }
-//
-//        return queryItems
-//    }
+    private static func map(data: Data?, response: URLResponse?, error: Error?) -> Result {
+        return Result{
+            if let error = error {
+                throw error
+            } else if let data, let response = response as? HTTPURLResponse {
+                return (data, response)
+            } else {
+                throw UnexpectedValuesRepresentation()
+            }
+        }
+    }
 }
