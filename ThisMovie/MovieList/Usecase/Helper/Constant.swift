04499b63d0770acfb61fe34daf2416b24bc9c508
Reynaldi <reynaldiwijaya2306@gmail.com>
2023-04-03 22:54:32 +0700

    Create `APIConstant` for constant values type that needed for making API Call.

---
+++ ThisMovie/MovieList/Usecase/Helper/Constant.swift
@@ -0,0 +1,14 @@
+//
+//  Constant.swift
+//  ThisMovie
+//
+//  Created by Reynaldi on 03/04/23.
+//
+
+import Foundation
+
+struct APIConstant {
+    static let DISCOVER_MOVIE_PATH = "/discover/movie"
+    static let LIST_GENRE_PATH = "/genre/movie/list"
+    static let API_KEY = "237abc9b06c308ee14cb2ed15957265f"
+}
